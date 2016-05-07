cbbf9c5c5b2cad5ee675be31801a1b9953dba8af
chenchangqing <chenchangqing198@126.com>
2016-05-07 17:43:22 +0800

    理解矩阵

--- _tutorial03_OpenGL基础知识/ogl/practice03_matrices/practice03.cpp
+++ _tutorial03_OpenGL基础知识/ogl/practice03_matrices/practice03.cpp
@@ -1,20 +1,169 @@
-// Include standard headers
+
 #include <stdio.h>
 #include <stdlib.h>
-
-// Include GLEW
 #include <GL/glew.h>
-
-// Include GLFW
 #include <glfw3.h>
-GLFWwindow* window;
-
-// Include GLM
+#include <common/shader.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
+/**
+ *  变量
+ */
+GLFWwindow* window;
+GLuint VAO;
+GLuint VBO;
+GLuint programID;
+GLuint MatrixID;
+glm::mat4 MVP;
+
+/**
+ *  顶点数据
+ */
+static const GLfloat g_vertex_buffer_data[] = {
+    -1.0f, -1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f,  1.0f, 0.0f,
+};
+
+/**
+ *  创建OpenGL窗口
+ */
+static int createWindow() {
+    
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        getchar();
+        return -1;
+    }
+    
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    window = glfwCreateWindow(1024, 768, "Practice 02", NULL, NULL);
+    
+    if (window == NULL)
+    {
+        fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    
+    return 0;
+}
+
+/**
+ *  创建矩阵
+ */
+static void createMatrix() {
+    
+    // Get a handle for our "MVP" uniform
+    MatrixID = glGetUniformLocation(programID, "MVP");
+    
+    // Projection matrix : 45∞ Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    // Or, for an ortho camera :
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+    
+    // Camera matrix
+    glm::mat4 View       = glm::lookAt(
+                                       glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+                                       glm::vec3(0,0,0), // and looks at the origin
+                                       glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                                       );
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model      = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+}
+
+/**
+ *  创建顶点数组缓冲
+ */
+static void createVBO()
+{
+    
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    programID = LoadShaders( "SimpleTransform.vertexshader", "SingleColor.fragmentshader" );
+    
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+}
+
+/**
+ *  渲染
+ */
+static void renderScene()
+{
+    
+    do{
+        glUseProgram(programID);
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+        
+        glClear( GL_COLOR_BUFFER_BIT );
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+    } while(glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
+    
+}
+
+/**
+ *  释放
+ */
+static void clear() {
+    
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(programID);
+    glfwTerminate();
+}
+
+/**
+ *  主函数
+ */
 int main( void )
 {
+    if (createWindow() != 0 ) {
+        
+        return -1;
+    }
+    
+    createVBO();
+    
+    createMatrix();
+    
+    renderScene();
+    
+    clear();
+    
     return 0;
 }
