9b0803ade8a968c743170c5e01534a3654440817
chenchangqing <chenchangqing198@126.com>
2016-05-21 22:37:26 +0800

    dev

--- _tutorial04_GLKit/CircleModel/CircleModel/Classes/JTCircleModel.m
+++ _tutorial04_GLKit/CircleModel/CircleModel/Classes/JTCircleModel.m
@@ -282,18 +282,23 @@ enum
     // mvp
     float aspect = fabs(self.bounds.size.width / self.bounds.size.height);
     
+    // 透视
 //    GLKMatrix4 mvMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -1.5f);
 //    GLKMatrix4 pMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(100), aspect, 0.1f, 3.9f);
 
+    // 正交
     const GLfloat zNear = 0.01, zFar = 1000.0, fieldOfView = 45.0;
     GLfloat size;
     size = zNear * tanf(GLKMathDegreesToRadians(fieldOfView) / 2.0);
     rect.size.width *= [UIScreen mainScreen].scale;
     rect.size.height *= [UIScreen mainScreen].scale;
-    
     GLKMatrix4 mvMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -5.0f);
     GLKMatrix4 pMatrix = GLKMatrix4MakeFrustum(-size, size, -size / (rect.size.width / rect.size.height), size /
                                                (rect.size.width / rect.size.height), zNear, zFar);
+    
+//    mvMatrix = GLKMatrix4Identity;
+//    pMatrix = GLKMatrix4Identity;
+    
     
     // 手势
     mvMatrix = GLKMatrix4Multiply(mvMatrix, GLKMatrix4MakeRotation(self.pitch, 1, 0, 0));
