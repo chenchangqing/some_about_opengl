73db27ad64d6809e577b4e3f3c0cdb4cdf174f65
chenchangqing <chenchangqing198@126.com>
2016-05-24 21:44:36 +0800

    dev

--- _tutorial04_GLKit/VRPlayer/VRPlayer/Classes/Models/UVCollection/UVCollection.m
+++ _tutorial04_GLKit/VRPlayer/VRPlayer/Classes/Models/UVCollection/UVCollection.m
@@ -15,8 +15,8 @@
     self = [super init];
     if (self) {
         
-        _rowCount = 4.0f;
-        _columnCount = 5.0f;
+        _rowCount = 2.0f;
+        _columnCount = 6.0f;
         
         _margin = 0.1f;
         _padding = 0.1f;
@@ -40,9 +40,7 @@
 - (void)draw {
     [super draw];
     
-    //    float itemW = (1.0f - _margin*2 - (_columnCount -1)*_columnSpace)/_columnCount;
-    //    float itemH = (1.0f - _margin*2 - (_rowCount -1)*_rowSpace)/_rowCount;
-    float itemW = 1.0f/_columnCount;
+    float itemW = (/** originW **/1.0f/_columnCount) * (/** aspect **/(1/(_columnCount*2 + (_columnSpace * (_columnCount - 1))) / (1/(_columnCount*2))));
     float itemH = 1.0f/_rowCount;
     
     float itemSx = itemW;
@@ -57,18 +55,26 @@
             // 如果是第一列，做连续位移(_columnCount-1)*2个单位
             if (column == 1) {
                 
+                // 找关系Horizontal
+                // 1 0   0
+                // 2 0.5 1
+                // 3 1   2
+                // 4 1.5 3
+                // 5 2   4
+                // 6 2.5 5
+                
                 // 如果是第一行，特殊处理
                 if (row == 1) {
                     
-                    super.modelViewMatrix = GLKMatrix4Translate(super.modelViewMatrix, 1-_columnCount, _rowCount-1, 0.0f);
+                    super.modelViewMatrix = GLKMatrix4Translate(super.modelViewMatrix, -(_columnCount-1)-_columnSpace*2.5, _rowCount-1, 0.0f);
                 } else {
                     
-                    super.modelViewMatrix = GLKMatrix4Translate(super.modelViewMatrix, -(_columnCount-1)*2, -2, 0.0f);
+                    super.modelViewMatrix = GLKMatrix4Translate(super.modelViewMatrix, -(_columnCount-1)*2-_columnSpace*5, -2, 0.0f);
                 }
                 
             } else {
                 
-                super.modelViewMatrix = GLKMatrix4Translate(super.modelViewMatrix, 2, 0, 0.0f);
+                super.modelViewMatrix = GLKMatrix4Translate(super.modelViewMatrix, 2+_columnSpace, 0, 0.0f);
             }
             [super draw];
         }
