1da3fbe8ca0162c93cc91845d26f17bad8f59fbe
chenchangqing <chenchangqing198@126.com>
2016-05-31 14:35:08 +0800

    dev

--- _tutorial04_GLKit/VRPlayer/VRPlayer/Classes/Models/UVModel/UVModel.m
+++ _tutorial04_GLKit/VRPlayer/VRPlayer/Classes/Models/UVModel/UVModel.m
@@ -58,6 +58,35 @@
 
 - (void)updateWithProjectionMatrix: (GLKMatrix4)projectionMatrix {
     
+//    GLKMatrix4 mtr = GLKMatrix4MakeTranslation(0, 0, 1);
+//    
+//    /**
+//     *  模型矩阵
+//     */
+//    GLKMatrix4 modelMatrix = GLKMatrix4Identity;
+//    
+//    modelMatrix = GLKMatrix4Scale(modelMatrix, _sx, _sy, _sz);// 缩放
+//    
+//    modelMatrix = GLKMatrix4Translate(modelMatrix, _tx, _ty, _tz);// 位移
+//    
+//    modelMatrix = GLKMatrix4Rotate(modelMatrix, GLKMathDegreesToRadians(_rx), 1.0f, 0.0f, 0.0f);
+//    modelMatrix = GLKMatrix4Rotate(modelMatrix, GLKMathDegreesToRadians(_ry), 0.0f, 1.0f, 0.0f);
+//    modelMatrix = GLKMatrix4Rotate(modelMatrix, GLKMathDegreesToRadians(_rz), 0.0f, 0.0f, 1.0f);// 旋转
+//    
+//    /**
+//     *  观察矩阵
+//     */
+//    GLKMatrix4 viewMatrix = GLKMatrix4Identity;
+//    
+//    viewMatrix = GLKMatrix4Rotate(viewMatrix, GLKMathDegreesToRadians(_yaw + _degree), 0.0f, 1.0f, 0.0f);
+//    viewMatrix = GLKMatrix4Rotate(viewMatrix, GLKMathDegreesToRadians(_pitch), 1.0f, 0.0f, 0.0f);
+//    
+//    /**
+//     *  投影矩阵
+//     */
+//    _projectionMatrix = projectionMatrix;
+//    _modelViewMatrix = GLKMatrix4Multiply(modelMatrix, viewMatrix);
+    
     // 初始位置(先绕y轴旋转,在绕x轴旋转)
     GLKMatrix4 positionMatrix = GLKMatrix4Identity;
     positionMatrix = GLKMatrix4Rotate(positionMatrix, GLKMathDegreesToRadians(_yaw + _degree), 0.0f, 1.0f, 0.0f);
